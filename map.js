365afd7d84b20f9a163004bb641a1b078e5db46f
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-29 19:17:01 +0600

    more map

--- map.js
+++ map.js
@@ -1,11 +1,21 @@
+//map ==> loops through each element of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array .....
 const numbers = [2, 34, 32, 5, 7];
 
 function doubleIt(num){
-    return num *2;
+    console.log('num now', num);
+    return num * 2;
 }
 
-const result = numbers.map(doubleIt);
+const double2 = n => n * 2;
 
+const result = numbers.map(doubleIt);
+// console.log(result);
+
+const output = numbers.map(double2);
+// console.log(output); 
+
+const output2 =numbers.map(n => n * 2);
+console.log(output2);
 
 // const doubled = [];
 // for(const num of numbers){
---
+++ more-map.js
@@ -0,0 +1,3 @@
+const numbers = [ 12, 3, 4, 23, 4];
+const doubled = numbers.map(num => num *2);
+console.log(doubled);
