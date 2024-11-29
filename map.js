d3b19bbc944ed6381409eda58a9eed142d3bb140
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-29 17:15:25 +0600

    map js

---
+++ map.js
@@ -0,0 +1,15 @@
+const numbers = [2, 34, 32, 5, 7];
+
+function doubleIt(num){
+    return num *2;
+}
+
+const result = numbers.map(doubleIt);
+
+
+// const doubled = [];
+// for(const num of numbers){
+//     const double = num * 2;
+//     doubled.push(double)
+// }
+// console.log(doubled);
