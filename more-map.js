769901fb787cf184afcdd8a9737169e9600f0559
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-29 19:45:18 +0600

    more

--- more-map.js
+++ more-map.js
@@ -1,3 +1,14 @@
 const numbers = [ 12, 3, 4, 23, 4];
 const doubled = numbers.map(num => num *2);
-console.log(doubled);
+// console.log(doubled);
+const fiveBonus = numbers.map(num => num + 5);
+// console.log(fiveBonus);
+
+const half = numbers.map(num=> num / 2);
+// console.log(half);
+
+const friends = ['faisal', 'haque', 'ayat', 'hosaain'];
+const lengths = friends.map(frnd => friends.length)
+// console.log(lengths);
+const firstLetter = friends.map(friend =>friends[3])
+console.log(firstLetter);
