effac70bffbb97fe381259a5056a8beed1cf3478
ismail-dev-code <ismail.online0385678@gmail.com>
2024-12-01 08:42:49 +0600

    more p

--- array/includes.js
+++ array/includes.js
@@ -1,11 +1,18 @@
-const friends = ['abul', 'dabul', 'gabul', 'modhu'];
-    console.log(friends.includes('dabul'));
+// const friends = ['abul', 'dabul', 'gabul', 'modhu'];
+//     console.log(friends.includes('dabul'));
 
-    if (friends.includes('dabul')){
-        console.log('party');
-    }
+//     if (friends.includes('dabul')){
+//         console.log('party');
+//     }
 
-    else{
-        console.log('no food , we are fasting');
-    }
+//     else{
+//         console.log('no food , we are fasting');
+//     }
 
+const friends = ['abul', 'babul', 'gabul', 'ratul'];
+
+console.log(friends.includes('bul'));
+if (friends.includes('bul')){
+    console.log('party hobe');
+}
+else{console.log('noooo food ');}
